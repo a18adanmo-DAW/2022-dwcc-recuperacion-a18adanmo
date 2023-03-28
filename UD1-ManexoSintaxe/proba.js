c883a53f209dad5329bd981fba6eef24a3354d91
a18adanmo <a18adanmo@iessanclemente.net>
2023-03-28 13:40:54 +0200

    28/Marzo/2023

---
+++ UD1-ManexoSintaxe/proba.js
@@ -0,0 +1,24 @@
+console.log("2" == 2);
+let a = 4, b = 5, c = "5";
+console.log("a = " + a + ", b = " + b + ", c = " + c);
+console.log("'b == c' -> " + (b == c));
+console.log("'b === c' -> " + (b === c));
+console.log("'b != c' -> " + (b != c));
+console.log("'b !== c' -> " + (b !== c));
+console.log("'a == b' -> " + (a == b));
+console.log("'a != b' -> " + (a != b));
+
+let favourite = Number(prompt('Cal é o teu número favorito?'));
+if (favourite === 5) {
+console.log('O 5 é un bo número');
+}
+
+console.log("'false && false' -> " + (false && false));
+console.log("'false && true' -> " + (false && true));
+console.log("'true && false' -> " + (true && false));
+console.log("'true && true' -> " + (true && true));
+console.log("'false || false' -> " + (false || false));
+console.log("'false || true' -> " + (false || true));
+console.log("'true || false' -> " + (true || false));
+console.log("'true || true' -> " + (true || true));
+console.log("'!false' -> " + !false);
